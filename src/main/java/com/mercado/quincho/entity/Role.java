5395d6e7e6289745110d5b5599c07a6e7518ee9e
ErikNaRV <enarvaez126@gmail.com>
2023-10-28 20:56:30 -0500

    Erik_Narvaez,Creacion de la entidad User

---
+++ src/main/java/com/mercado/quincho/entity/Role.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mercado.quincho.entity;
+
+/**
+ *
+ * @author erik_
+ * @version 3.4.1 jdk11 28/10/2023
+ * Enum contenedor de los roles que se establece en los requisitos 
+ * (cliente,propietario,ADMIN)
+ * 
+ */
+public enum Role {
+    
+    CUSTOMER,
+    OWNER,
+    ADMIN;
+    
+}
