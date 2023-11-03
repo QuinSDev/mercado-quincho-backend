6f4bfb1e8115a1e7acb0e3944997af9d7223f79c
QuinSDev <quindev97@gmail.com>
2023-11-02 21:44:13 -0500

    Sebastian_Quintero, se modifica la entidad Service agregandole el javadoc y nuevos atributos que representan los servicios disponibles de un quincho.

--- src/main/java/com/mercado/quincho/entity/Service.java
+++ src/main/java/com/mercado/quincho/entity/Service.java
@@ -4,35 +4,45 @@
  */
 package com.mercado.quincho.entity;
 
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 /**
- *
- * @author Adan Suarez
+ * Entidad que representa los servicios disponibles en un quincho.
+ * Esta clase define la estructura de los servicios, incluyendo atributos
+ * que representan sevicios como wifi, estacionamiento, aire acondicionado,
+ * piscina, etc.
+ * 
+ * @author QuinSDev
  */
-@Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Service {
+
     @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id_serv;
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(length = 36) // Cambia la longitud a 36 caracteres para UUID
+    private String id_serv;
 
-        private Boolean wifi;
-        private Boolean parking;
-        private Boolean airCon;
-        private Boolean pool;
-        private Boolean television;
-        private Boolean washingMach;
-        private Boolean kitchen;
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id_quincho;
+    private Boolean wifi;
+    private Boolean parking;
+    private Boolean airConditioning;
+    private Boolean pool;
+    private Boolean television;
+    private Boolean washingMachine;
+    private Boolean kitchen;
+    private Boolean refrigerator;
+    private Boolean microwave;
+    private Boolean kitchenUtensil;
+       
 }
