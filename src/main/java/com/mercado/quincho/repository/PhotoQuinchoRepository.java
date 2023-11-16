2c9be80ea30a5a90ff74cbccb8bd5e92cac7b0ae
QuinSDev <quindev97@gmail.com>
2023-11-16 18:25:12 -0500

    SebastianQ, Se documenta el código en formato Javadoc y se cambia el nombre del método a findByQuinchoId

--- src/main/java/com/mercado/quincho/repository/PhotoQuinchoRepository.java
+++ src/main/java/com/mercado/quincho/repository/PhotoQuinchoRepository.java
@@ -6,12 +6,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- *
+ * Repositorio que gestiona la entidad PhotoQuincho en la base de datos.
+ * Proporciona métodos para realizar operaciones relacionadas con las fotos de
+ * los quinchos.
+ * 
  * @author QuinSDev
  */
 @Repository
 public interface PhotoQuinchoRepository extends JpaRepository<PhotoQuincho, String>{
-
-    public List<PhotoQuincho> findByQuinchoIdQuincho(String idQuincho);
+    
+    /**
+     * Busca y devuelve una lista de fotos de quinchos por el ID del quincho.
+     * 
+     * @param idQuincho: El ID del quincho del que se quieren obtener las fotos.
+     * @return Lista de fotos asociadas al quincho identificado por el ID.
+     */
+    public List<PhotoQuincho> findByQuinchoId(String idQuincho);
     
 }
