0ac03164569db3d9a78642283c220a28ee3e469e
QuinSDev <quindev97@gmail.com>
2023-11-22 19:06:23 -0500

    SebastianQ, Se actualiza el controlador de CustomerOpinion para registrar una opinion y obtener las opiniones de un quincho

--- src/main/java/com/mercado/quincho/controller/CustomerOpinionController.java
+++ src/main/java/com/mercado/quincho/controller/CustomerOpinionController.java
@@ -5,10 +5,14 @@
 package com.mercado.quincho.controller;
 
 import com.mercado.quincho.entity.CustomerOpinion;
+import com.mercado.quincho.request.OpinionRequest;
+import com.mercado.quincho.response.QuinchoResponse;
 import com.mercado.quincho.service.CustomerOpinionService;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,63 +28,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/customer-opinions")
+@RequiredArgsConstructor
 public class CustomerOpinionController {
-    /*
-    private final CustomerOpinionService customerOpinionService;
-
+    
     @Autowired
-    public CustomerOpinionController(CustomerOpinionService 
-           customerOpinionService) {
-        this.customerOpinionService = customerOpinionService;
-    }*/
-
-    /**
-     * Obtiene una opinión de cliente por su ID.
-     *
-     * @param id El ID de la opinión del cliente.
-     * @return Un Optional que contiene la CustomerOpinion si se 
-     * encuentra, o vacío si no se encuentra.
-     */
-    /*@GetMapping("/{id}")
-    public Optional<CustomerOpinion> getCustomerOpinionById(@PathVariable
-           String id) {
-        return customerOpinionService.findCustomerOpinionById(id);
-    }*/
-
-    /**
-     * Obtiene opiniones de clientes por su puntuación (qualification).
-     *
-     * @param qualification La puntuación por la cual buscar opiniones 
-     * de clientes.
-     * @return Una lista de CustomerOpinion que coinciden con la 
-     * puntuación proporcionada.
-     */
-    /*@GetMapping("/by-qualification/{qualification}")
-    public List<CustomerOpinion> getCustomerOpinionsByQualification
-        (@PathVariable int qualification) {
-        return customerOpinionService.findCustomerOpinionsByQualification
-        (qualification);
+    private final CustomerOpinionService customerOpinionService;
+            
+    @PostMapping(value = "register/{idUser}/{idQuincho}")
+    public ResponseEntity<QuinchoResponse> registerOpinion(OpinionRequest request,
+            @PathVariable String idUser, @PathVariable String idQuincho) {
+        return ResponseEntity.ok(customerOpinionService.registerOpinion(idUser, idQuincho, request));
     }
-
-    /**
-     * Agrega una nueva CustomerOpinion.
-     *
-     * @param customerOpinion La CustomerOpinion a agregar.
-     * @return La CustomerOpinion agregada.
-     */
-    /*@PostMapping
-    public CustomerOpinion addCustomerOpinion(@RequestBody CustomerOpinion 
-           customerOpinion) {
-        return customerOpinionService.saveCustomerOpinion(customerOpinion);
+    
+    @GetMapping(value = "quincho/{idQuincho}")
+    public ResponseEntity<List<CustomerOpinion>> getListOpinionsQuincho(@PathVariable String idQuincho)
+    {
+        return ResponseEntity.ok(customerOpinionService.getListOpinionsQuincho(idQuincho));
     }
-
-    /**
-     * Elimina una CustomerOpinion por su ID.
-     *
-     * @param id El ID de la opinión del cliente a eliminar.
-     */
-    /*@DeleteMapping("/{id}")
-    public void deleteCustomerOpinion(@PathVariable String id) {
-        customerOpinionService.deleteCustomerOpinionById(id);
-    }*/
 }
